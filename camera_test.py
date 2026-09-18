d484f79213268dc90cf0fb9b13b501d8c098c36a
Anup Kumar Singh <anup94685@gmail.com>
2026-09-19 02:52:37 +0530

    Add files via upload

---
+++ camera_test.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Cannot access camera")
+        break
+
+    cv2.imshow("Camera Test", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
